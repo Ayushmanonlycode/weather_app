b95d73ea14aad01ac6868690430acf173bc02e49
Ayushmanonlycode <ayushman.onlycode69@gmail.com>
2025-06-07 19:09:10 +0530

    added some fix.2

--- lib/service-worker.ts
+++ lib/service-worker.ts
@@ -1,7 +1,7 @@
 export function registerServiceWorker() {
   if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('/sw.js')
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
